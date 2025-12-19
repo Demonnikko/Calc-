1c94850f78bf175444b5d4fdedb2ce6d3c26a36a
Demonnikko <petrovd060@gmail.com>
2025-12-20 01:23:15 +0300

    Add files via upload

---
+++ kostyuk_crm_pwa_package/sw.js
@@ -0,0 +1,39 @@
+/* Kostyuk CRM – simple offline cache */
+const CACHE = 'kostyuk-crm-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './sw.js',
+  './icons/icon-192.png',
+  './icons/icon-512.png',
+  './icons/apple-touch-icon-180.png',
+  './icons/apple-touch-icon-167.png',
+  './icons/apple-touch-icon-152.png',
+  './icons/apple-touch-icon-120.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE).then((cache) => cache.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => (k === CACHE ? null : caches.delete(k)))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  const req = event.request;
+  if (req.method !== 'GET') return;
+  event.respondWith(
+    caches.match(req).then((cached) => cached || fetch(req).then((resp) => {
+      const copy = resp.clone();
+      caches.open(CACHE).then((cache) => cache.put(req, copy));
+      return resp;
+    }).catch(() => caches.match('./index.html')))
+  );
+});
